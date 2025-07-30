40593ed7096e97b4af2c807710d5e71198a7bd8f
Jacob <hortonjacob1@gmail.com>
2025-07-30 01:23:19 +0100

    style: cleaner inline code block styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,28 @@ module.exports = {
         },
       },
 
-      // Get rid of quotation marks from blockquotes
       typography: {
         DEFAULT: {
           css: {
+            // Remove quotation marks from blockquotes
             "blockquote p:first-of-type::before": false,
             "blockquote p:first-of-type::after": false,
+
+            // Re-style inline code blocks
+            code: {
+              backgroundColor: '#e9eaeb',
+              padding: '0.2rem 0.4rem',
+              borderRadius: '0.25rem',
+            },
+
+            // Slightly darker inline code block background when inside blockquote
+            'blockquote code': {
+              backgroundColor: '#d9dae0',
+            },
+
+            // Remove quotation marks from inline code blocks
+            'code::before': { content: 'none' },
+            'code::after': { content: 'none' },
           },
         },
       },
